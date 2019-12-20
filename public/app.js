88a4def3a706de908ff686e97490cb83c7c47815
Luke Nemy <lukenemy7@gmail.com>
2019-12-19 16:09:50 -0800

    connected firestore, used .get and .onSnapshot to retrieve data

--- public/app.js
+++ public/app.js
@@ -1,6 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const app = firebase.app();
-    console.log(app)
+  const app = firebase.app();
+
+  const db = firebase.firestore();
+
+  const myPost = db.collection('posts').doc('firstpost');
+
+
+  /* Get a post/ record from the firestore/db */
+  // myPost.get()
+  //   .then(doc => {
+  //     const data = doc.data();
+  //     document.write( data.title + `<br>`);
+  //     document.write( data.createdAt );
+  //     document.write( data.views );
+  //   })
+  /* onSnapshot will automatically update the page when data in db changes
+  - useful for realtime scenaros
+  */
+  myPost.onSnapshot(doc => {
+    const data = doc.data();
+      document.write( data.title + `<br>`);
+      document.write( data.createdAt );
+      document.write( data.views );
+  })
+
+
 });
 
 function googleLogin() {
