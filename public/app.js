d0bec48042fcc2466c80e8aaf1d8b1acf696a89e
Luke Nemy <lukenemy7@gmail.com>
2019-12-19 16:35:45 -0800

    added simple firebase query of multiple documents from single collection

--- public/app.js
+++ public/app.js
@@ -4,25 +4,46 @@ document.addEventListener('DOMContentLoaded', function() {
   const db = firebase.firestore();
 
   const myPost = db.collection('posts').doc('firstpost');
+  const productsRef = db.collection('products');
 
+  const query = productsRef.where('price', '>', 10)
+
+  query.get()
+    .then( products => {
+      products.forEach(  doc => {
+        data =  doc.data();
+        document.write(`${data.name} at $${data.price} <br>`);
+  })
+      })
 
   /* Get a post/ record from the firestore/db */
+
   // myPost.get()
   //   .then(doc => {
   //     const data = doc.data();
   //     document.write( data.title + `<br>`);
-  //     document.write( data.createdAt );
-  //     document.write( data.views );
+  //     document.write( data.createdAt + `<br>`);
+  //     document.write( data.views + `<br>`);
   //   })
+
+
   /* onSnapshot will automatically update the page when data in db changes
   - useful for realtime scenaros
   */
-  myPost.onSnapshot(doc => {
-    const data = doc.data();
-      document.write( data.title + `<br>`);
-      document.write( data.createdAt );
-      document.write( data.views );
-  })
+
+  // myPost.onSnapshot(doc => {
+  //   const data = doc.data();
+  //     document.write( data.title + `<br>`);
+  //     document.write( data.createdAt );
+  //     document.write( data.views );
+  // })
+
+
+  /* for live updating with form input */
+  // myPost.onSnapshot( doc => {
+  //   const data = doc.data();
+  //   document.querySelector('#title').innerHTML = data.title
+  // })
 
 
 });
@@ -36,4 +57,10 @@ function googleLogin() {
       document.write(`Hello ${user.displayName}`);
       console.log(user)
     })
+}
+
+function updatePost(e) {
+  const db = firebase.firestore();
+  const myPost = db.collection('posts').doc('firstpost');
+  myPost.update({ title: e.target.value })
 }
